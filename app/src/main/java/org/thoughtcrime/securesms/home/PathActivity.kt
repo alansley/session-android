7804ea0eff9a4834585237acf462ab5f0b1a2ede
alansley <aclansley@gmail.com>
2024-02-19 10:53:49 +1100

    Replaced

--- app/src/main/java/org/thoughtcrime/securesms/home/PathActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/home/PathActivity.kt
@@ -99,7 +99,7 @@ class PathActivity : PassphraseRequiredActionBarActivity() {
                 val isGuardSnode = (OnionRequestAPI.guardSnodes.contains(snode))
                 getPathRow(snode, LineView.Location.Middle, index.toLong() * 1000 + 2000, dotAnimationRepeatInterval, isGuardSnode)
             }
-            val youRow = getPathRow(resources.getString(R.string.activity_path_device_row_title), null, LineView.Location.Top, 1000, dotAnimationRepeatInterval)
+            val youRow = getPathRow(resources.getString(R.string.onionRoutingPath), null, LineView.Location.Top, 1000, dotAnimationRepeatInterval)
             val destinationRow = getPathRow(resources.getString(R.string.activity_path_destination_row_title), null, LineView.Location.Bottom, path.count().toLong() * 1000 + 2000, dotAnimationRepeatInterval)
             val rows = listOf( youRow ) + pathRows + listOf( destinationRow )
             for (row in rows) {
